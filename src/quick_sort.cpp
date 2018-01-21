bf93713eb42cf2dcbfc7d5f7578225f21953a107
ArchieGertsman <gertsman63@gmail.com>
2018-01-21 17:20:39 -0600

    Update quick_sort.cpp

--- src/quick_sort.cpp
+++ src/quick_sort.cpp
@@ -4,27 +4,27 @@
 #include <map>
 
 std::vector<int> &append(std::vector<int> &v1, const std::vector<int> &v2) {
-	v1.insert(v1.end(), v2.begin(), v2.end());
-	return v1;
+    v1.insert(v1.end(), v2.begin(), v2.end());
+    return v1;
 }
 
 std::vector<int> quickSort(const std::vector<int> &v) {
-	int pivot = v[v.size() / 2];
-	std::vector<int> chunk1, chunk2, pivots;
+    int pivot = v[v.size() / 2];
+    std::vector<int> chunk1, chunk2, pivots;
 
-	for (const auto &a : v) {
-		if (a == pivot) {
-			pivots.push_back(a); continue;
-		}
-		(a <= pivot ? chunk1 : chunk2).push_back(a);
-	}
+    for (const auto &a : v) {
+        if (a == pivot) {
+            pivots.push_back(a); continue;
+        }
+        (a <= pivot ? chunk1 : chunk2).push_back(a);
+    }
 
-	if (chunk1.size() > 1) chunk1 = quickSort(chunk1);
-	if (chunk2.size() > 1) chunk2 = quickSort(chunk2);
+    if (chunk1.size() > 1) chunk1 = quickSort(chunk1);
+    if (chunk2.size() > 1) chunk2 = quickSort(chunk2);
 
-	return append(append(chunk1, pivots), chunk2);
+    return append(append(chunk1, pivots), chunk2);
 }
 
 int main() {
-  auto sorted_list = quickSort({ 15, 35, 91, 11, 27, 71, 65, 47 });
+    auto sorted_list = quickSort({ 15, 35, 91, 11, 27, 71, 65, 47 });
 }
