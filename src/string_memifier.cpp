1c2ec3fb8b2145f1364ee0856994c01821bc84b0
ArchieGertsman <gertsman63@gmail.com>
2018-01-22 22:08:24 -0600

    Create string_memifier.cpp

---
+++ src/string_memifier.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <random>   
+#include <algorithm>
+    
+int randBinDig() {
+    std::random_device rand;
+    std::mt19937 mt(rand());
+    std::uniform_int_distribution<int> dist(0, 1);
+    return dist(mt);
+}
+
+int main() {
+    std::string meme;
+    std::getline(std::cin, meme);
+    std::transform(meme.begin(), meme.end(), meme.begin(), [&](char &c) { 
+        return randBinDig() == 0 ? c : std::toupper(c);    
+    });
+    std::cout << meme << '\n';
+}
